515c4af22f98f1104e679ea18aff7bcf7b6f1f52
O Daniel <soumenorgame@gmail.com>
2021-12-02 20:34:02 -0300

    chore: starter project

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const getUrlTeste = () => "https://flaviocopes.com/ecmascript/";
+
+export const getUrlTesteTwo = () => "https://flaviocopes.com/ecmascript/";
+
+export const sum = (first: number, second: number) => first + second;
---
+++ src/sample.ts
@@ -0,0 +1,5 @@
+import * as channel from "./index";
+
+console.log(channel.getUrlTeste());
+console.log(channel.getUrlTesteTwo());
+console.log(channel.sum(20, 20));
