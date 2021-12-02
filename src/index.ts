93e69c05a1917b735e8cf7ea85211642c9dbbeb4
O Daniel <soumenorgame@gmail.com>
2021-12-02 20:36:08 -0300

    chore: remove files test

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export const getUrlTeste = () => "https://flaviocopes.com/ecmascript/";
-
-export const getUrlTesteTwo = () => "https://flaviocopes.com/ecmascript/";
-
-export const sum = (first: number, second: number) => first + second;
--- src/sample.ts
+++
@@ -1,5 +0,0 @@
-import * as channel from "./index";
-
-console.log(channel.getUrlTeste());
-console.log(channel.getUrlTesteTwo());
-console.log(channel.sum(20, 20));
